850166e0412f156c97b292a1c3e00fa694261087
sarwar-asik <sarwarasik@gmail.com>
2024-07-27 01:39:13 +0600

    build: apps built done @build

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+            manifest: {
+                name: "Study Live",
+                short_name: "Study Live",
+                description: "this is a live video app.",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                ],
+            },
+        }),
+    ],
+    server: {
+        hmr: {
+            overlay: true,
+        },
+    },
+    resolve: {
+        alias: {
+            "@": "/src",
+            "@modules": "/src/Pages/Modules",
+        },
+    },
+});
