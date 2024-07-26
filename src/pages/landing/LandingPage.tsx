f663df2ef18aa3f550ed2e750971f7b9a6581814
sarwar-asik <sarwarasik@gmail.com>
2024-07-27 01:20:49 +0600

    feature-add: #included landing-page  @feature

---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <React.Fragment><section className="py-12 bg-white sm:py-16 lg:py-20">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 md:items-center gap-y-8 md:grid-cols-2 md:gap-x-16">
+                    <div className="text-center md:text-left lg:pr-16">
+                        <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">
+                            Video calls and meetings for everyone
+                        </h2>
+                        <p className="mt-4 text-base text-gray-700 sm:mt-8 font-pj">
+                            Connect, collaborate, and celebrate from anywhere with Study Live video , live message streamming
+                        </p>
+                        <div className="relative inline-flex gap-5 mt-10 group">
+                            <button className='p-5 bg-primary text-white font-semibold rounded border border-primary h-[3rem] py-2'>New Meeting +</button>
+                            <div className=" bg-white">
+                                <input
+                                    type="url"
+                                    name=""
+                                    id=""
+                                    placeholder="Insert a Link"
+                                    // defaultValue="Insert a Link"
+                                    className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 border rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600 outline-none"
+                                />
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="w-full mx-auto"
+                            src="https://cdn.rareblocks.xyz/collection/clarity/images/features/2/illustration.png"
+                            alt=""
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+        </React.Fragment>
+    )
+}
--- src/router/MainRouterPath.tsx
+++ src/router/MainRouterPath.tsx
@@ -1,6 +1,7 @@
 import Login from "@/pages/Auth/Login";
 import SignUp from "@/pages/Auth/SignUp";
 import Home from "@/pages/home/Home";
+import LandingPage from "@/pages/landing/LandingPage";
 import PricingPage from "@/pages/pricing/PricingPage";
 
 export const MainRouterPath = [
@@ -19,5 +20,9 @@ export const MainRouterPath = [
     {
         path: "/pricing",
         element: <PricingPage />
+    },
+    {
+        path: "/landing",
+        element: <LandingPage />
     },
 ]
