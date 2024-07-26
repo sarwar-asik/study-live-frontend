73aa5f0e8f2ea294bd0fbbbe346f24c6df953ef3
sarwar-asik <sarwarasik@gmail.com>
2024-07-27 01:29:40 +0600

    feature-update: #included few ui  @update-ui

--- src/pages/error/NotFound.tsx
+++ src/pages/error/NotFound.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 export default function NotFound() {
   return (
-    <React.Fragment>NotFound Page</React.Fragment>
+    <React.Fragment><>
+      {/* component */}
+      <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+        <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+          <div className="relative">
+            <div className="absolute">
+              <div className="">
+                <h1 className="my-2 text-gray-800 font-bold text-2xl">
+                  Looks like you've found the doorway to the great nothing
+                </h1>
+                <p className="my-2 mb-9 text-gray-800">
+                  Sorry about that! Please visit our hompage to get where you need
+                  to go.
+                </p>
+                <Link to="/" className="sm:w-full lg:w-auto mt-[5rem] border rounded md py-4 px-8 text-center bg-primary text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50">
+                  Go Home
+                </Link>
+              </div>
+            </div>
+            <div>
+              <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
+        </div>
+      </div>
+    </>
+    </React.Fragment>
   )
 }
